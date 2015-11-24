fbf5dc602d3daa5c9fd8f8772f66ceef1be4b932
StephanieBeaton <stefanybeaton@gmail.com>
2015-11-24 00:12:38 +0000

    8. adding new post functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'posts' => 'posts#index'
+  get 'posts/new', to: 'posts#new'
 
   # if a request comes in with "/posts/192023675"
   # ... or anything else after "/posts", it can handle it no problem.
@@ -9,6 +10,8 @@ Rails.application.routes.draw do
   # ... it knows to capture that number in params[:id]
   # ... and pass it through to the appropriate controller.
   get 'posts/:id', to: 'posts#show'
+  post 'posts',    to: 'posts#create'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
